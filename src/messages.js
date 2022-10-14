ea257768b4a93a4c0541d1a79e9d1db289c8657a
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-10-14 12:12:38 -0400

    change Custom CSS and JS to Fluent UI for VSCode

--- src/messages.js
+++ src/messages.js
@@ -1,16 +1,15 @@
 exports.messages = {
 	admin: "Run VS Code with admin privileges so the changes can be applied.",
 	enabled:
-		"Custom CSS and JS enabled. Restart to take effect. " +
-		"If Code complains about it is corrupted, CLICK DON'T SHOW AGAIN. " +
-		"See README for more detail.",
-	disabled: "Custom CSS and JS disabled and reverted to default. Restart to take effect.",
-	already_disabled: "Custom CSS and JS already disabled.",
+		"Fluent UI for VSCode is enabled. Restart to take effect. " +
+		"If Code complains that it is corrupted, CLICK DON'T SHOW AGAIN.",
+	disabled: "Fluent UI for VSCode disabled and reverted to default. Restart to take effect.",
+	already_disabled: "Fluent UI for VSCode already disabled.",
 	somethingWrong: "Something went wrong: ",
 	restartIde: "Restart Visual Studio Code",
-	notfound: "Custom CSS and JS not found.",
+	notfound: "Fluent UI for VSCode not found.",
 	notConfigured:
-		"Custom CSS and JS path not configured. " +
+		"Fluent UI for VSCode path not configured. " +
 		'Please set "vscode_custom_css.imports" in your user settings.',
 	reloadAfterVersionUpgrade:
 		"Detected reloading CSS / JS after VSCode is upgraded. " + "Performing application only.",
